f4b40a1f9d069659bfafb0096b587580edd82e3e
GAUTAM PALADIYA <mrpaladiyagautam@gmail.com>
2021-03-07 05:56:06 +0100

    Email register added and other will be remove

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,54 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const UserModel = require("../models/ModelUser");
-const CircularJSON = require("circular-json");
+var jwt = require("jsonwebtoken");
 
 router.post("/register", (req, res) => {
-  res.send({ msg: "Registerd done successfull" });
+  console.log(`email register ${JSON.stringify(req.body)}`);
+  UserModel.findOne({ email: req.body.email })
+    .exec()
+    .then(async (users) => {
+      console.log(`users ${users}`);
+      if (!users) {
+        console.log("not found");
+
+        const newUser = new UserModel({ ...req.body });
+
+        newUser.save().then(
+          async (suc) => {
+            console.log(`user save ${suc}`);
+            const data = {
+              id: suc._id,
+              email: suc.email,
+              name: suc.name,
+              signUpType: suc.signUpType,
+            };
+            const token = await jwt.sign(
+              JSON.stringify(data),
+              process.env.SECRET
+            );
+            res.send({
+              msg: "User created successfully",
+              token: token,
+            });
+          },
+          (err) => {
+            console.log(`user save err ${err}`);
+            res.status(403).send({ msg: "Server Error Try again" });
+          }
+        );
+      } else {
+        console.log("not else");
+        const data = {
+          id: users._id,
+          email: users.email,
+          name: users.name,
+          signUpType: users.signUpType,
+        };
+        const token = await jwt.sign(JSON.stringify(data), process.env.SECRET);
+        res.send({ msg: "User allready registered", token: token });
+      }
+    });
 });
 
 router.post("/fbRegister", async (req, res) => {
